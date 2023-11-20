b791a185a249f32b6340290355d1413898a0c38a
s809 <43530948+s809@users.noreply.github.com>
2023-11-20 16:54:17 +0500

    Deduplicate code in MultiplayerConfigChangeListener

--- core/src/main/java/com/seibel/distanthorizons/core/multiplayer/config/MultiplayerConfigChangeListener.java
+++ core/src/main/java/com/seibel/distanthorizons/core/multiplayer/config/MultiplayerConfigChangeListener.java
@@ -1,35 +1,37 @@
 package com.seibel.distanthorizons.core.multiplayer.config;
 
 import com.seibel.distanthorizons.core.config.Config;
+import com.seibel.distanthorizons.core.config.types.ConfigEntry;
 import com.seibel.distanthorizons.core.config.listeners.ConfigChangeListener;
 
 import java.io.Closeable;
+import java.util.ArrayList;
 
+@SuppressWarnings({"rawtypes", "unchecked"})
 public class MultiplayerConfigChangeListener implements Closeable
 {
-	private final ConfigChangeListener<Integer> renderDistanceRadius;
-	private final ConfigChangeListener<Boolean> enableDistantGeneration;
-	private final ConfigChangeListener<Integer> requestRateLimit;
-	private final ConfigChangeListener<Boolean> enableRealTimeUpdates;
-	private final ConfigChangeListener<Boolean> enablePostRelogUpdate;
+	private static final ConfigEntry[] CONFIG_ENTRIES = new ConfigEntry[] {
+			Config.Client.Advanced.Graphics.Quality.lodChunkRenderDistanceRadius,
+			Config.Client.Advanced.WorldGenerator.enableDistantGeneration,
+			Config.Client.Advanced.Multiplayer.ServerNetworking.requestRateLimit,
+			Config.Client.Advanced.Multiplayer.ServerNetworking.enableRealTimeUpdates,
+			Config.Client.Advanced.Multiplayer.ServerNetworking.enablePostRelogUpdate
+	};
+	
+	private final ArrayList<ConfigChangeListener> changeListeners = new ArrayList<>();
 	
 	public MultiplayerConfigChangeListener(Runnable runnable)
 	{
-		renderDistanceRadius = new ConfigChangeListener<>(Config.Client.Advanced.Graphics.Quality.lodChunkRenderDistanceRadius, ignored -> runnable.run());
-		enableDistantGeneration = new ConfigChangeListener<>(Config.Client.Advanced.WorldGenerator.enableDistantGeneration, ignored -> runnable.run());
-		requestRateLimit = new ConfigChangeListener<>(Config.Client.Advanced.Multiplayer.ServerNetworking.requestRateLimit, ignored -> runnable.run());
-		enableRealTimeUpdates = new ConfigChangeListener<>(Config.Client.Advanced.Multiplayer.ServerNetworking.enableRealTimeUpdates, ignored -> runnable.run());
-		enablePostRelogUpdate = new ConfigChangeListener<>(Config.Client.Advanced.Multiplayer.ServerNetworking.enablePostRelogUpdate, ignored -> runnable.run());
+		for (ConfigEntry entry : CONFIG_ENTRIES)
+			changeListeners.add(new ConfigChangeListener(entry, ignored -> runnable.run()));
 	}
 	
 	@Override
 	public void close()
 	{
-		renderDistanceRadius.close();
-		enableDistantGeneration.close();
-		requestRateLimit.close();
-		enableRealTimeUpdates.close();
-		enablePostRelogUpdate.close();
+		for (ConfigChangeListener changeListener : changeListeners)
+			changeListener.close();
+		changeListeners.clear();
 	}
 	
 }
