f92a1ccc27cb6ffaca70431a27120a20148dbafa
s809 <43530948+s809@users.noreply.github.com>
2024-11-14 15:52:41 +0500

    Add a comment to #pluginMessageReceived methods

--- core/src/main/java/com/seibel/distanthorizons/core/api/internal/ClientApi.java
+++ core/src/main/java/com/seibel/distanthorizons/core/api/internal/ClientApi.java
@@ -25,7 +25,7 @@ import com.seibel.distanthorizons.api.enums.rendering.EDhApiRenderPass;
 import com.seibel.distanthorizons.api.methods.events.abstractEvents.*;
 import com.seibel.distanthorizons.api.methods.events.sharedParameterObjects.DhApiRenderParam;
 import com.seibel.distanthorizons.core.file.structure.ClientOnlySaveStructure;
-import com.seibel.distanthorizons.core.logging.f3.F3Screen;
+import com.seibel.distanthorizons.core.network.messages.MessageRegistry;
 import com.seibel.distanthorizons.core.pos.DhChunkPos;
 import com.seibel.distanthorizons.core.render.DhApiRenderProxy;
 import com.seibel.distanthorizons.core.render.renderer.FadeRenderer;
@@ -356,6 +356,11 @@ public class ClientApi
 	// networking //
 	//============//
 	
+	/**
+	 * Forwards a decoded message into the registered handlers.
+	 *
+	 * @see MessageRegistry
+	 */
 	public void pluginMessageReceived(@NotNull AbstractNetworkMessage message)
 	{
 		NetworkSession networkSession = this.pluginChannelApi.networkSession;
