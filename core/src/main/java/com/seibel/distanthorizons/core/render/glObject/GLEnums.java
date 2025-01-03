770fc10cdf6cac5ecf88780ecacb13e3da738633
James Seibel <jeseibel@gondtc.com>
2025-01-03 15:58:27 -0600

    Add missing types to GLEnums.getString()

--- core/src/main/java/com/seibel/distanthorizons/core/render/glObject/GLEnums.java
+++ core/src/main/java/com/seibel/distanthorizons/core/render/glObject/GLEnums.java
@@ -211,6 +211,28 @@ public class GLEnums
 			default:
 		}
 		
+		// Types
+		switch (glEnum)
+		{
+			case GL_BYTE:
+				return "GL_BYTE";
+			case GL_UNSIGNED_BYTE:
+				return "GL_UNSIGNED_BYTE";
+			case GL_SHORT:
+				return "GL_SHORT";
+			case GL_UNSIGNED_SHORT:
+				return "GL_UNSIGNED_SHORT";
+			case GL_INT:
+				return "GL_INT";
+			case GL_UNSIGNED_INT:
+				return "GL_UNSIGNED_INT";
+			case GL_FLOAT:
+				return "GL_FLOAT";
+			case GL_DOUBLE:
+				return "GL_DOUBLE";
+			default:
+		}
+		
 		return "GL_UNKNOWN(" + glEnum + ")";
 	}
 	
